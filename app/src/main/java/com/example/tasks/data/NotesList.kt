d93a36415d3a0d51d7720cfffe656b1ed62604a7
orlogue <102894674+orlogue@users.noreply.github.com>
2023-02-01 06:13:39 +1000

    Changed model

--- app/src/main/java/com/example/tasks/data/NotesList.kt
+++ app/src/main/java/com/example/tasks/data/NotesList.kt
@@ -3,7 +3,6 @@ package com.example.tasks.data
 data class NotesList(
     var id: Int = UNDEFINED_ID,
     var name: String,
-    var notes: List<Note>
 ) {
     companion object {
         const val UNDEFINED_ID = -1

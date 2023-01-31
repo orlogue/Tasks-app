a09b48df0b08d36de44cdf49f881721d96553dc9
orlogue <102894674+orlogue@users.noreply.github.com>
2023-02-01 06:17:04 +1000

    Added viewpager2 adapter

---
+++ app/src/main/java/com/example/tasks/presentation/ui/PagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.tasks.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tasks.data.NotesList
+
+class PagerAdapter(
+    private val parentFragment: FragmentActivity,
+    private val notesList: List<NotesList>
+) : FragmentStateAdapter(parentFragment) {
+
+    override fun getItemCount(): Int {
+        return notesList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val fragment = NotesFragment()
+
+        fragment.arguments = Bundle().apply {
+            putInt(LIST_ID, notesList[position].id)
+        }
+        return fragment
+    }
+
+    companion object {
+        const val LIST_ID = "list-id"
+    }
+}

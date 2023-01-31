b0e8c2eab8eab2be3be2f11c40ba421860cffbd6
orlogue <102894674+orlogue@users.noreply.github.com>
2023-02-01 06:14:33 +1000

    Implemented repository

--- app/src/main/java/com/example/tasks/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/tasks/data/RepositoryImpl.kt
@@ -1,51 +1,84 @@
 package com.example.tasks.data
 
+import com.example.tasks.data.db.dao.NotesDao
+import com.example.tasks.data.db.models.NoteDbEntity
+import com.example.tasks.data.db.models.NotesListDbEntity
 import com.example.tasks.domain.Repository
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class RepositoryImpl : Repository {
-    private val lists: MutableList<NotesList> = mutableListOf()
-    private val notes: MutableList<Note> = mutableListOf()
-
-
-    override fun createList(list: NotesList) {
-        lists.add(list)
-    }
-
-    override fun renameList(list: NotesList) {
-        lists.find { it.id == list.id }?.name = list.name
-    }
-
-    override fun deleteList(listId: Int) {
-        lists.removeIf { it.id == listId }
-    }
-
-    override fun getLists(): List<NotesList> {
-        return lists.toList()
-    }
-
-    override fun getNote(id: Int): Note? {
-        return notes.find { it.id == id }
-    }
-
-    override fun addNote(note: Note) {
-        notes.add(note)
-    }
-
-    override fun editNote(note: Note) {
-        notes.find { it == note }?.apply {
-            title = note.title
-            description = note.description
-            listId = note.listId
-            isFavorite = note.isFavorite
-            isCompleted = note.isCompleted
+class RepositoryImpl(
+    private val notesDao: NotesDao,
+    private val ioDispatcher: CoroutineDispatcher
+) : Repository {
+    override suspend fun initDatabase() {
+        CoroutineScope(ioDispatcher).launch {
+//            notesDao.deleteLists()
+//            withContext(Dispatchers.Main) {
+                notesDao.initDatabase()
+//            }
         }
     }
 
-    override fun deleteNote(id: Int) {
-        notes.removeIf { it.id == id }
+    override suspend fun createList(list: NotesList) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.createList(NotesListDbEntity.toNotesListDbEntity(list))
+        }
     }
 
-    override fun getNotesList(listId: Int): List<Note> {
-        return notes.filter { it.listId == listId}
+    override suspend fun renameList(list: NotesList) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.renameList(NotesListDbEntity.toNotesListDbEntity(list))
+        }
+    }
+
+    override suspend fun deleteList(list: NotesList) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.deleteList(NotesListDbEntity.toNotesListDbEntity(list))
+        }
+    }
+
+    override fun getLists(): Flow<List<NotesList>> {
+        val list: Flow<List<NotesList>>
+//        val waitFor = CoroutineScope(ioDispatcher).async {
+            list = notesDao.getLists().map { it.map { it.toNotesList() } }
+//            return@async list
+//        }
+        return list
+    }
+
+    override suspend fun getNote(id: Int): Note? {
+        val waitFor = CoroutineScope(ioDispatcher).async {
+            return@async notesDao.getNoteById(id)?.toNote()
+        }
+        return waitFor.await()
+    }
+
+    override suspend fun addNote(note: Note) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.createNote(NoteDbEntity.toNoteDbEntity(note))
+        }
+    }
+
+    override suspend fun editNote(note: Note) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.updateNote(NoteDbEntity.toUpdateNoteDbEntity(note))
+        }
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        CoroutineScope(ioDispatcher).launch {
+            notesDao.deleteNote(NoteDbEntity.toNoteDbEntity(note))
+        }
+    }
+
+    override fun getNotesList(): Flow<List<Note>> {
+        val list: Flow<List<Note>>
+//        val waitFor = CoroutineScope(ioDispatcher).async {
+            list = notesDao.getNotes().map { it.map { it.toNote() } }
+//            return@async list
+//        }
+        return list
     }
 }

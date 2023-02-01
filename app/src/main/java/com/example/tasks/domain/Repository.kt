effaecbb9c4adc512122e93589cd13da433fa977
orlogue <102894674+orlogue@users.noreply.github.com>
2023-02-02 02:46:04 +1000

    Changed signature and deleted method

--- app/src/main/java/com/example/tasks/domain/Repository.kt
+++ app/src/main/java/com/example/tasks/domain/Repository.kt
@@ -5,16 +5,14 @@ import com.example.tasks.data.NotesList
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
-    suspend fun initDatabase()
     suspend fun createList(list: NotesList)
     suspend fun renameList(list: NotesList)
     suspend fun deleteList(listId: Int)
     fun getLists(): Flow<List<NotesList>>
 
-    suspend fun getNote(id: Int): Note?
     suspend fun addNote(note: Note)
     suspend fun editNote(note: Note)
-    suspend fun deleteNote(note: Note)
+    suspend fun deleteNote(noteId: Int)
     suspend fun deleteCompletedNotes(listId: Int)
     fun getNotesList(): Flow<List<Note>>
 }

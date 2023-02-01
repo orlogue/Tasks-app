dfb08e2762ad83e5cc359f3aa84ff08323909bc4
orlogue <102894674+orlogue@users.noreply.github.com>
2023-02-02 02:44:44 +1000

    Made class parcelable

--- app/src/main/java/com/example/tasks/data/Note.kt
+++ app/src/main/java/com/example/tasks/data/Note.kt
@@ -1,5 +1,9 @@
 package com.example.tasks.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Note(
     var listId: Int,
     var title: String,
@@ -8,7 +12,7 @@ data class Note(
     var isFavorite : Boolean = false,
     var isCompleted : Boolean = false,
     val id: Int = UNDEFINED_ID
-) {
+) : Parcelable {
     companion object {
         const val UNDEFINED_ID = -1
     }
